225792ac0b248ec83df90b6ab93d5f907f2ef45f
Verrickt <Adios_Verrickt@hotmail.com>
2021-09-28 14:17:24 +0800

    Update headers

--- build.ps1
+++ build.ps1
@@ -5,4 +5,7 @@ Copy-Item -Path './conference/types.yml' -Destination (Join-Path $dest_dir 'type
 $all_yml = Get-Content './conference/*/*.yml' -Encoding UTF8
 Set-Content -Path (Join-Path $dest_dir 'allconf.yml') $all_yml -Encoding UTF8 -Force
 
+$last_updated = Get-ChildItem -Path(Join-Path $dest_dir 'allconf.yml') | select LastWriteTimeUtc
+Set-Content -Path (Join-Path $dest_dir 'last_updated.json') $last_updated.LastWriteTimeUtc -Encoding UTF8 -Force
+
 Write-Host $running_mode
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,7 @@
   <section>
     <el-row>
       <a href="/" class="title">AISec Conference Deadlines</a>
-      <github-button style="padding-left: 5px"></github-button>
-      <span v-if="showLatestConf" style="color:#fd3c95;font-weight: bold;">Latest: {{this.showStr}} !!!</span>
+      <span v-if="showLatestConf" style="color:#fd3c95;font-weight: bold; margin: 0 20px">Last Updated: {{this.showStr}}</span>
     </el-row>
     <el-row class="subtitle">
       Countdowns to AISec conference deadlines. To add/edit a conference, <a style="color: #666666" href="https://github.com/ccfddl/ccf-deadlines/pulls">send a pull request.</a>
@@ -12,17 +11,28 @@
 </template>
 
 <script>
-// import GithubButton from './GithubButton'
-// export default {
-//   name: 'Header',
-//   components: {GithubButton},
-//   data() {
-//     return {
-//       showLatestConf: false,
-//       showStr: ''
-//     }
-//   },
-// }
+const moment = require('moment')
+export default {
+  name: 'Header',
+  data() {
+    return {
+      showLatestConf: true,
+      showStr: ''
+    }
+  },
+  mounted() {
+    this.$http.get('/conference/last_updated.json').then(
+      response=>{
+        console.log(response)
+        console.log(response.bodyText)
+        let last_modified_utc = moment.utc(response.bodyText)
+        let last_modified_local = last_modified_utc.local()
+        let last_modified_str = last_modified_local.format('ddd MMM Do YYYY HH:mm:ss z')
+        this.showStr = last_modified_str
+      }
+    )
+  },
+}
 </script>
 
 <style scoped>
