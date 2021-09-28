6fb2538a51174beaf7d199075a5c1b264d6820f5
Verrickt <Adios_Verrickt@hotmail.com>
2021-09-28 11:10:47 +0800

    Fix  static resource path

--- build.ps1
+++ build.ps1
@@ -1,17 +1,4 @@
-param ($running_mode)
-$valid = "dev","deploy"
-if ($running_mode -notin $valid) {
-    throw "Invalid running mode ${running_mode}. Valid Options are [${valid}]"
-}
-$dest_dir
-if($running_mode -eq 'dev')
-{
-    $dest_dir = './src'
-}
-else 
-{
-    $dest_dir = './public'
-}
+$dest_dir = './public'
 $dest_dir = Join-Path $dest_dir 'conference' 
 New-Item -Type dir $dest_dir -Force
 Copy-Item -Path './conference/types.yml' -Destination (Join-Path $dest_dir 'types.yml') -Force
