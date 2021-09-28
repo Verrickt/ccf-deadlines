aab48e2bdb609870f2c1134fb6002fa557379811
Verrickt <Adios_Verrickt@hotmail.com>
2021-09-28 11:03:29 +0800

    Fix encoding

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 param ($running_mode)
 $valid = "dev","deploy"
 if ($running_mode -notin $valid) {
-    Write-Error "Invalid running mode ${running_mode}. Valid Options are [${valid}]"
+    throw "Invalid running mode ${running_mode}. Valid Options are [${valid}]"
 }
 $dest_dir
 if($running_mode -eq 'dev')
@@ -14,8 +14,8 @@ else
 }
 $dest_dir = Join-Path $dest_dir 'conference' 
 New-Item -Type dir $dest_dir -Force
-Copy-Item -Path './conference/types.yml' -Destination (Join-Path $dest_dir 'types.yml') 
-$all_yml = Get-Content './conference/*/*.yml'
-Set-Content -Path (Join-Path $dest_dir 'allconf.yml') $all_yml
+Copy-Item -Path './conference/types.yml' -Destination (Join-Path $dest_dir 'types.yml') -Force
+$all_yml = Get-Content './conference/*/*.yml' -Encoding UTF8
+Set-Content -Path (Join-Path $dest_dir 'allconf.yml') $all_yml -Encoding UTF8 -Force
 
 Write-Host $running_mode
